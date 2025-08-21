b90d689f8de2d7b75e30f754111ec48e75f2d257
yantr-manav <saivamshijilla04@gmail.com>
2025-08-22 00:18:17 +0530

    changes in frontend code

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
+@app.route('/main')
+def main():
+    return render_template('main.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=True)
